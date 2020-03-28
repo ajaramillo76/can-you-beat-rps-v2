544640df3eeba4c7f7f127533cf6df3c4e857f8f
Your Name Comes Here <archie808@hotmail.com>
2020-03-27 22:51:16 -0600

    Added Google Sign in.

--- app/src/main/java/edu/cnm/deepdive/canyoubeatrps/CanYouBeatRpsApplication.java
+++ app/src/main/java/edu/cnm/deepdive/canyoubeatrps/CanYouBeatRpsApplication.java
@@ -1,18 +1,15 @@
 package edu.cnm.deepdive.canyoubeatrps;
 
 import android.app.Application;
+import edu.cnm.deepdive.canyoubeatrps.service.GoogleSignInRepository;
+
 
 public class CanYouBeatRpsApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    GoogleSignInRepository.setContext(this);
     Stetho.initializeWithDefaults(this);
-    edu.cnm.deepdive.canyoubeatrps.service.GoogleSignInRepository.setContext(this);
-    CanYouBeatRpsDatabase.setContext(this);
-    CanYouBeatRpsDatabase.getInstance().getGameDao().delete()
-        .subscribeOn(Schedulers.io())
-        .subscribe();
-
   }
 }
