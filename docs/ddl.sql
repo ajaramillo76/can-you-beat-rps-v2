8f98f4f49a8a2e640501c97e2643feedd9209214
Your Name Comes Here <archie808@hotmail.com>
2020-03-31 09:24:44 -0600

    Updated ddl.

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -32,30 +32,3 @@ CREATE INDEX IF NOT EXISTS `index_Game_rock` ON `Game` (`rock`);
 CREATE INDEX IF NOT EXISTS `index_Game_paper` ON `Game` (`paper`);
 
 CREATE INDEX IF NOT EXISTS `index_Game_scissors` ON `Game` (`scissors`);
-
-CREATE TABLE IF NOT EXISTS `HistoricalData`
-(
-    `historicaldata_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    `user_id`           INTEGER                           NOT NULL,
-    `wins`              INTEGER                           NOT NULL,
-    `losses`            INTEGER                           NOT NULL,
-    `draw`              INTEGER                           NOT NULL,
-    `rock`              INTEGER                           NOT NULL,
-    `paper`             INTEGER                           NOT NULL,
-    `scissors`          INTEGER                           NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON UPDATE NO ACTION ON DELETE CASCADE
-);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_user_id` ON `HistoricalData` (`user_id`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_wins` ON `HistoricalData` (`wins`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_losses` ON `HistoricalData` (`losses`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_draw` ON `HistoricalData` (`draw`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_rock` ON `HistoricalData` (`rock`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_paper` ON `HistoricalData` (`paper`);
-
-CREATE INDEX IF NOT EXISTS `index_HistoricalData_scissors` ON `HistoricalData` (`scissors`);
